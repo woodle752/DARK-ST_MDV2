3590ddb82309a8a9eab3945049dd876b009c7674
Dark-S-TEN <darksten4@gmail.com>
2024-08-13 14:38:47 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,107 @@
+
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "sten10royal@gmail.com";
+global.location = "Port-au-Prince,Haïti.";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Hamza:3800380ww@cluster0.uwommwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "50931461936@s.whatsapp.net";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "America/Port-au-Prince";
+global.github = process.env.GITHUB || "";
+global.gurl = process.env.GURL || "https://chat.whatsapp.com/Hxduja6rDRh30gSNeUNyfm";
+global.website = process.env.GURL || "https://chat.whatsapp.com/Hxduja6rDRh30gSNeUNyfm";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/3e1d82ab139d29975cad4.jpg";
+global.devs = "50931461936";
+global.sudo = process.env.SUDO || " ";
+global.owner = process.env.OWNER_NUMBER || "50931461936";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "";
+global.waPresence = process.env.WAPRESENCE || "available";
+global.readcmds = process.env.READ_COMMAND || "true";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
+global.read_status_from = process.env.READ_STATUS_FROM || "all";
+
+global.api_smd = "https://HyHamza.vercel.app";
+global.scan = "https://byte-session.vercel.app/";
+
+
+
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+
+
+
+
+
+  
+
+
+//___________________________________________________________________________________________________
+
+
+  
+ "wap mete session id ou a la 🔪🔪💔" //Make sure session id starts with Byte;;;
+
+
+
+//________________________________________________________________________
+
+
+
+
+
+
+
+
+
+module.exports = {
+  menu: process.env.MENU || "",
+
+  //Prefix variable
+  HANDLERS: process.env.PREFIX || ",",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "*Powered By 𝙎-𝙏𝞢𝞜*",
+  author: process.env.PACK_AUTHER || "DRK-ST-MD ",
+  packname: process.env.PACK_NAME || "𝙎-𝙏𝞢𝞜",
+  botname: process.env.BOT_NAME || "DRK-ST-MD",
+  ownername: process.env.OWNER_NAME || "𝙎-𝙏𝞢𝞜",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "BYTE").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});
